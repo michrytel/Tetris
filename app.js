ac124e079223b92d70c71dcc0b49d6ed5994ad3d
michrytel <76482065+michrytel@users.noreply.github.com>
2021-03-19 15:35:17 +0100

    RWD

--- app.js
+++ app.js
@@ -245,7 +245,3 @@ document.querySelector(".mobile__rotate").addEventListener('click', function(e) 
 document.querySelector(".mobile__down").addEventListener('click', function(e) {
     moveDown()
 });
-
-document.addEventListener('touchmove', function (event) {
-    if (event.scale !== 1) { event.preventDefault(); }
-}, { passive: false });
